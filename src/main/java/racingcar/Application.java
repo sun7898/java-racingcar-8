715dde2e493e45262dc3a08d0bb7290d4cb04e4f
sun7898 <jhs15767@gmail.com>
2025-10-27 21:45:45 +0900

    feat: 메인 경주 시작점 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String[] names = InputView.readCarNames();
+        int attemptCount = InputView.readAttemptCount();
+
+        List<Car> cars = Arrays.stream(names)
+                .map(Car::new)
+                .toList();
+
+        RacingGame game = new RacingGame(cars, attemptCount);
+        game.play();
+
+        OutputView.printWinners(game.getWinners());
     }
 }
